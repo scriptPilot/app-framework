2f5e27c2af774a58690c2fb649a1a377150acaf9
scriptPilot <mail@scriptPilot.de>
2016-12-20 07:44:20 +0100

    build 0.10.1

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Load files
-const pkg = require('./package.json')
+var pkg = require('./package.json')
 //var project = require('json!./project.temp')
 //var app = require('json!./app.temp')
 var project = require(pkg.projectPath + 'package.json');
