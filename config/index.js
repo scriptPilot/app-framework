9d76b6a5eeeb9e228cbe574200346738fe9a15a4
scriptPilot <mail@scriptPilot.de>
2017-01-15 13:36:34 +0100

    source maps configurable

--- config/index.js
+++ config/index.js
@@ -1,14 +1,19 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 var path = require('path')
 
+// TODO appPath
+var appPath = '../'
+
+var pkg = require(appPath + 'package.json')
+
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    index: path.resolve(__dirname, appPath + 'www/build-' + pkg.version + '/index.html'),
+    assetsRoot: path.resolve(__dirname, appPath + 'www/build-' + pkg.version),
+    assetsSubDirectory: '',
     assetsPublicPath: '',
-    productionSourceMap: true,
+    productionSourceMap: pkg.buildSourcemaps === true ? true : false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
