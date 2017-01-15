098872ee1b2ac3370161eb8bb5511a5cdaec9e03
scriptPilot <mail@scriptPilot.de>
2017-01-15 21:09:16 +0100

    postinstall routine

---
+++ postinstall.js
@@ -0,0 +1,28 @@
+var isThere = require('is-there')
+
+// app-framework is installed as dependency
+if (isThere('../../package.json')) {
+  
+  var saveJSON = require('jsonfile')
+  saveJSON.spaces = 2  
+  
+  var pkg = require('./package.json')
+  
+  pkg.projectRoot = '../../'
+  pkg.appRoot = '../../'
+  
+  saveJSON.writeFileSync('./package.json', pkg)
+
+  var cpx = require('cpx')
+  
+  if (!isThere('../../images')) {
+    cpx.copy('demo-app/images/*', '../../images')
+  }
+  if (!isThere('../../pages')) {
+    cpx.copy('demo-app/pages/*', '../../pages')
+  }
+  if (!isThere('../../app.vue')) {
+    cpx.copy('demo-app/app.vue', '../../')
+  }
+  
+}
