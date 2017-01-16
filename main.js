2551cd24dbea5a9f8bcf24c86a128d32bf9f47e6
scriptPilot <mail@scriptPilot.de>
2017-01-16 13:07:11 +0100

    import > require

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 // Import Vue
-import Vue from 'vue'
+var Vue = require('vue')
 
 // Import F7
-import Framework7 from 'framework7'
+var Framework7 = require('framework7')
 
 // Import F7 Vue Plugin
-import Framework7Vue from 'framework7-vue'
+var Framework7Vue = require('framework7-vue')
 
 // Import F7 iOS Theme Styles
-import Framework7Theme from 'framework7/dist/css/framework7.ios.min.css'
-import Framework7ThemeColors from 'framework7/dist/css/framework7.ios.colors.min.css'
+var Framework7Theme = require('framework7/dist/css/framework7.ios.min.css')
+var Framework7ThemeColors = require('framework7/dist/css/framework7.ios.colors.min.css')
 /* OR for Material Theme:
 import Framework7Theme from 'framework7/dist/css/framework7.material.min.css'
 import Framework7ThemeColors from 'framework7/dist/css/framework7.material.colors.min.css'
