14215d15d15d9a6e971a9ab6995f9bf28c6eed4a
scriptPilot <mail@scriptPilot.de>
2017-01-24 23:20:12 +0100

    clean-up

---
+++ build/cleanupFirebaseDeploy.js
@@ -0,0 +1,11 @@
+var pkg = require('../package.json')
+var app = require('..' + pkg.appRoot + 'package.json')
+
+var isThere = require('is-there')
+var deleteFiles = require('delete')
+var path = require('path')
+
+}
+}
+
+module.exports = {}
