f7ab95e8f83648122b6e2e07f1ca7a087eef1a25
scriptPilot <mail@scriptPilot.de>
2017-01-24 23:20:15 +0100

    clean-up

--- build/cleanupFirebaseDeploy.js
+++ build/cleanupFirebaseDeploy.js
@@ -5,7 +5,11 @@ var isThere = require('is-there')
 var deleteFiles = require('delete')
 var path = require('path')
 
+if (isThere(path.resolve(__dirname, '..' + pkg.appRoot + '.firebaserc'))) {
+  deleteFiles.sync([path.resolve(__dirname, '..' + pkg.appRoot + '.firebaserc')])
 }
+if (isThere(path.resolve(__dirname, '..' + pkg.appRoot + 'firebase.json'))) {
+  deleteFiles.sync([path.resolve(__dirname, '..' + pkg.appRoot + 'firebase.json')])
 }
 
 module.exports = {}
