cb622f06707732fd432f42e3f893e21cc755db45
scriptPilot <mail@scriptPilot.de>
2017-01-29 16:19:03 +0100

    wip

---
+++ scripts/f7vue-update.js
@@ -0,0 +1,26 @@
+// Load configuration
+var cfg = require('../config.js')
+
+// Load packages
+var path = require('path')
+var isThere = require('is-there')
+var run = require('child_process').exec
+var ora = require('ora')
+
+// Framework7-Vue folder exists
+let f7vueFolder = path.resolve(cfg.packageRoot, '..', 'Framework7-Vue')
+if (isThere(f7vueFolder)) {
+  
+  // Build
+  ora('Build Framework7-Vue ...').start()
+  run('cd "' + f7vueFolder + '" && npm run build', function (err, stdOut, errOut) {
+    if (!err) {
+      console.log('Build done')
+    } else {
+      console.log(errOut)
+    }
+  })
+  
+} else {
+  console.log('Error: Framework7-Vue folder not found')
+}
