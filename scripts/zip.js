89e18c9ced24a9d84f761e08e842a9ebd871e97e
scriptPilot <mail@scriptPilot.de>
2017-01-30 15:56:37 +0100

    minor bump

--- scripts/zip.js
+++ scripts/zip.js
@@ -9,12 +9,11 @@ var app = require(cfg.appRoot + 'package.json')
 
 // Installed as package
 if (cfg.isInstalled) {
-
   // Show message
   showOnly('ZIP file creation ongoing - please wait ...')
 
-  let zipFilename = app.name + '_' + (new Date()).getFullYear() + '-' + ((new Date()).getMonth()+1 < 10 ? '0' : '') + ((new Date()).getMonth()+1) + '-' + ((new Date()).getDate() < 10 ? '0' : '') + (new Date()).getDate() + '.zip'
-  zipdir('./demo-app', { saveTo: zipFilename, filter: function (filepath, stat) {  
+  let zipFilename = app.name + '_' + (new Date()).getFullYear() + '-' + ((new Date()).getMonth() + 1 < 10 ? '0' : '') + ((new Date()).getMonth() + 1) + '-' + ((new Date()).getDate() < 10 ? '0' : '') + (new Date()).getDate() + '.zip'
+  zipdir('./demo-app', { saveTo: zipFilename, filter: function (filepath, stat) {
     let lastBuildFolder = path.join('www', 'build-' + app.devDependencies['app-framework'].substr(1))
     filepath = filepath.substr(path.resolve(cfg.appRoot).length + 1)
     return (filepath.substr(0, 3) !== 'www' && filepath.substr(filepath.length - 3, 3) !== 'zip')
@@ -29,7 +28,7 @@ if (cfg.isInstalled) {
       showOnly('Error: Zip file creation failed')
     }
   })
-  
+
 // Not installed as package
 } else {
   showOnly('Error: Zip file can be created only after App Framework is installed as package')
