64908300baefaab697b2a1d73ddf40f58b6177e6
scriptPilot <mail@scriptPilot.de>
2017-01-30 15:59:14 +0100

    js conformity

--- scripts/zip.js
+++ scripts/zip.js
@@ -13,15 +13,18 @@ if (cfg.isInstalled) {
   showOnly('ZIP file creation ongoing - please wait ...')
 
   let zipFilename = app.name + '_' + (new Date()).getFullYear() + '-' + ((new Date()).getMonth() + 1 < 10 ? '0' : '') + ((new Date()).getMonth() + 1) + '-' + ((new Date()).getDate() < 10 ? '0' : '') + (new Date()).getDate() + '.zip'
-  zipdir('./demo-app', { saveTo: zipFilename, filter: function (filepath, stat) {
-    let lastBuildFolder = path.join('www', 'build-' + app.devDependencies['app-framework'].substr(1))
-    filepath = filepath.substr(path.resolve(cfg.appRoot).length + 1)
-    return (filepath.substr(0, 3) !== 'www' && filepath.substr(filepath.length - 3, 3) !== 'zip')
-        || filepath === 'www'
-        || filepath === path.join('www/.htaccess')
-        || filepath === lastBuildFolder
-        || filepath.substr(0, lastBuildFolder.length + 1) === lastBuildFolder + path.sep
-  }}, function (err, buffer) {
+  zipdir('./demo-app', {
+    saveTo: zipFilename,
+    filter: function (filepath, stat) {
+      let lastBuildFolder = path.join('www', 'build-' + app.devDependencies['app-framework'].substr(1))
+      filepath = filepath.substr(path.resolve(cfg.appRoot).length + 1)
+      return (filepath.substr(0, 3) !== 'www' && filepath.substr(filepath.length - 3, 3) !== 'zip') ||
+              filepath === 'www' ||
+              filepath === path.join('www/.htaccess') ||
+              filepath === lastBuildFolder ||
+              filepath.substr(0, lastBuildFolder.length + 1) === lastBuildFolder + path.sep
+    }
+  }, function (err, buffer) {
     if (!err) {
       showOnly('Project files and latest build saved to "' + zipFilename + '". You can backup that ZIP file to an external drive or cloud now.')
     } else {
