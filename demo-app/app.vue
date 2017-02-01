244a0b861cc2b8bc113c728c8e7da81ac854f165
scriptPilot <mail@scriptPilot.de>
2017-02-01 11:51:29 +0100

    Update app.vue

--- demo-app/app.vue
+++ demo-app/app.vue
@@ -1,52 +1,53 @@
+<!-- Template for app component -->
 <template>
-  <!-- App -->
+
+  <!-- Will replace app container -->
   <div id="app">
   
-    <!-- Status bar -->
+    <!-- Statusbar -->
     <f7-statusbar></f7-statusbar>
 
-    <!-- Left Panel -->
+    <!-- Left panel -->
     <f7-panel left reveal layout="dark">
-      <f7-view id="left-panel-view" navbar-through :dynamic-navbar="true" url="panel-left">
-      </f7-view>
+      <f7-view id="left-panel-view" navbar-through :dynamic-navbar="true" url="panel-left"></f7-view>
     </f7-panel>
 
-    <!-- Right Panel -->
+    <!-- Right panel -->
     <f7-panel right cover layout="dark">
-      <f7-view id="right-panel-view" navbar-through :dynamic-navbar="true" url="panel-right">
-      </f7-view>
+      <f7-view id="right-panel-view" navbar-through :dynamic-navbar="true" url="panel-right"></f7-view>
     </f7-panel>
 
-    <!-- Main Views -->
+    <!-- Main view -->
     <f7-views>
-      <f7-view id="main-view" navbar-through :dynamic-navbar="true" main url="home">
-      </f7-view>
+      <f7-view id="main-view" navbar-through :dynamic-navbar="true" main url="home"></f7-view>
     </f7-views>
 
     <!-- Popup -->
     <f7-popup id="popup">
-      <f7-view id="popup" navbar-fixed url="popup">
-      </f7-view>
+      <f7-view id="popup" navbar-fixed url="popup"></f7-view>
     </f7-popup>
 
     <!-- Login Screen -->
     <f7-login-screen id="login-screen">
-      <f7-view id="login-screen" url="login-screen">
-      </f7-view>
+      <f7-view id="login-screen" url="login-screen"></f7-view>
     </f7-login-screen>
 
   </div>
 </template>
 
+<!-- Default export, template will be compiled automatically -->
 <script>
-module.exports = {}
+  module.exports = {}
 </script>
 
+<!-- Style - for whole app, as there is no scope property -->
 <style>
+
   /* Icon sizes */
   .item-media .fa,
   .swipeout-actions-left .fa,
   .swipeout-actions-right .fa  {
     font-size: 20px;
   }
+  
 </style>
