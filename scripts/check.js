c96c381b7abfa9bc2b9fafcdb7feb61cba052c7b
scriptPilot <mail@scriptPilot.de>
2017-02-05 21:11:24 +0100

    path fix
    solve #133

--- scripts/check.js
+++ scripts/check.js
@@ -3,6 +3,7 @@ var isThere = require('is-there')
 var run = require('child_process').exec
 var showOnly = require('./show-only')
 var deleteFiles = require('delete').sync
+var path = require('path')
 
 // Load configuration
 var cfg = require('./config.js')
@@ -10,9 +11,13 @@ var cfg = require('./config.js')
 // Show message
 showOnly('Standard JavaScript fix ongoing - please wait ...')
 
-// Do the fix
-var files = cfg.isInstalled ? 'app.vue" "pages/*.vue"' : '"scripts/*.js" "demo-app/app.vue" "demo-app/pages/*.vue"'
-run('standard >standard-check.log ' + files + ' --plugin html', function (err, stdOut, errOut) {
+// Define files to check
+var files = ' "' + path.resolve(cfg.appRoot, 'app.vue') + '"' +
+            ' "' + path.resolve(cfg.appRoot, 'pages/*.vue') + '"' +
+            cfg.isInstalled ? '' : ' "' + path.resolve(cfg.packageRoot, 'scripts/*.js') + '"'
+
+// Do the check
+run('standard >' + path.resolve(cfg.projectRoot, 'standard-check.log') + ' ' + files + ' --plugin html', function (err, stdOut, errOut) {
   if (err) {
     throw new Error('Standard JavaScript check failed - please check "standard-check.log" for detailed information or run "npm run fix".')
   } else {
