5dc4f395a59f460cc8b886889eaf42f4ba9e3f6e
scriptPilot <mail@scriptPilot.de>
2017-02-09 22:09:15 +0100

    Cordova plugins

--- scripts/run.js
+++ scripts/run.js
@@ -2,6 +2,10 @@ let childProcess = require('child_process')
 
 module.exports = function (command, callbackOnSuccess) {
   childProcess.exec(command, function (err, stdOut, errOut) {
+    // Skip specific errors
+    if (/(.*)The plugin '(.+) is required by/.test(errOut)) {
+      err = undefined
+    }
     if (!err) {
       if (typeof callbackOnSuccess === 'function') {
         callbackOnSuccess()
