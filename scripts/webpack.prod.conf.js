0d79eaf2738f68bf705416c99d48c49a43595389
scriptPilot <mail@scriptPilot.de>
2017-02-16 14:46:48 +0100

    fix

--- scripts/webpack.prod.conf.js
+++ scripts/webpack.prod.conf.js
@@ -123,8 +123,8 @@ var webpackConfig = merge(baseWebpackConfig, {
 
       // Save browserconfig file
       let xml = '<?xml version="1.0" encoding="utf-8"?>' +
-                '<browserconfig>'
-      '  <msapplication>' +
+                '<browserconfig>' +
+                '  <msapplication>' +
                 '    <tile>' +
                 '      <square150x150logo src="mstile-150x150.png"/>' +
                 '      <TileColor>#da532c</TileColor>' +
