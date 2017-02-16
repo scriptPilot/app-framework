7a67ab0baad8c1df34963bea4146ea0cc79d8c52
scriptPilot <mail@scriptPilot.de>
2017-02-16 17:23:58 +0100

    adaption

--- scripts/backup.js
+++ scripts/backup.js
@@ -3,10 +3,10 @@ var path = require('path')
 var run = require('./run')
 var cmd = require('interactive-command')
 var showOnly = require('./show-only')
-var write = require('write')
-var saveJSON = require('jsonfile')
+//var write = require('write')
+//var saveJSON = require('jsonfile')
 var isThere = require('is-there')
-saveJSON.spaces = 2
+//saveJSON.spaces = 2
 
 // Load configuration
 var cfg = require('./config.js')
@@ -17,11 +17,14 @@ run('node "' + path.resolve(cfg.packageRoot, 'scripts/prepare-firebase') + '"', 
 
   cmd('./node_modules/.bin/firebase', ['login'], {cwd: cfg.projectRoot}, function () {
     showOnly('Backup Firebase database - please wait ...')
-    cmd('firebase', ['database:get', '/'], {cwd: path.resolve(cfg.projectRoot, 'node_modules/firebase-tools/bin')}, function (res) {
+    cmd('firebase', ['database:get', '/', '>../../../database-backup.json'], {cwd: path.resolve(cfg.projectRoot, 'node_modules/firebase-tools/bin')}, function (res) {
+      /*
       if (!isThere(cfg.appRoot + 'database-backup.json')) {
         write.sync(cfg.appRoot + 'database-backup.json', '{}')
       }
+      console.log("###", res, "###")
       saveJSON.writeFileSync(cfg.appRoot + 'database-backup.json', JSON.parse(res))
+      */
       showOnly('Clean up - please wait ...')
       run('node "' + path.resolve(cfg.packageRoot, 'scripts/cleanup-firebase') + '"', function () {
         showOnly('Firebase database backup done!')
