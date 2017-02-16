387638278fdacfbfdaf629b973d23464e84e8f75
scriptPilot <mail@scriptPilot.de>
2017-02-16 14:03:41 +0100

    modify icon handling
    close #213

--- scripts/webpack.dev.conf.js
+++ scripts/webpack.dev.conf.js
@@ -5,7 +5,6 @@ var path = require('path')
 var webpack = require('webpack')
 var merge = require('webpack-merge')
 var utils = require('./utils')
-var list = require('list-dir')
 var baseWebpackConfig = require('./webpack.base.conf')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 
@@ -13,22 +12,6 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 Object.keys(baseWebpackConfig.entry).forEach(function (name) {
   baseWebpackConfig.entry[name] = ['./scripts/dev-client'].concat(baseWebpackConfig.entry[name])
 })
-
-// Define icon tags
-let iconTags = ''
-let iconFiles = []
-let icons = list.sync(path.resolve(cfg.packageRoot, 'icons'))
-for (let i = 0; i < icons.length; i++) {
-  if (/^icon-([0-9]+)\.([0-9]+)\.png/.test(icons[i])) {
-    let size = icons[i].match(/^icon-([0-9]+)\.([0-9]+)\.png/)[1]
-    iconTags += '<link rel="icon" type="image/png" size="' + size + 'x' + size + '" href="' + (cfg.isInstalled ? 'node_modules/app-framework/' : '') + 'icons/' + icons[i] + '" />'
-    iconFiles.push(icons[i])
-  } else if (/^apple-touch-icon-([0-9]+)\.([0-9]+)\.png/.test(icons[i])) {
-    let size = icons[i].match(/^apple-touch-icon-([0-9]+)\.([0-9]+)\.png/)[1]
-    iconTags += '<link rel="apple-touch-icon" type="image/png" size="' + size + 'x' + size + '" href="' + (cfg.isInstalled ? 'node_modules/app-framework/' : '') + 'icons/' + icons[i] + '" />'
-    iconFiles.push(icons[i])
-  }
-}
 
 module.exports = merge(baseWebpackConfig, {
   module: {
@@ -46,7 +29,7 @@ module.exports = merge(baseWebpackConfig, {
       filename: 'index.html',
       template: 'index.ejs',
       title: app.title,
-      iconTags: iconTags,
+      iconTags: '', /* favicon.ico will be loaded by browser default from root directory */
       manifest: '',
       inject: true
     })
