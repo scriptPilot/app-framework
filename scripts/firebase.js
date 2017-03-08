04b10a7650695e03a960524798bf11a8b48f169c
scriptPilot <mail@scriptPilot.de>
2017-03-08 22:53:18 +0100

    rework

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -11,9 +11,15 @@
 // Include modules
 let env = require('../env')
 let alert = require('../lib/alert')
+let cmd = require('../lib/cmd')
 let found = require('../lib/found')
 let fs = require('fs-extra')
+let join = require('path').join
+let rel = require('path').relative
 let abs = require('path').resolve
+
+// Run
+alert('Firebase deployment ongoing - please wait ...')
 
 // Check arguments
 if (env.arg.database !== true && env.arg.storage !== true && env.arg.hosting !== true) {
@@ -21,7 +27,7 @@ if (env.arg.database !== true && env.arg.storage !== true && env.arg.hosting !==
 }
 
 // Prepare Firebase deployment
-let cacheFolder = abs(env.cache, 'firebase')
+let binFolder = abs(env.proj, 'node_modules/firebase-tools/bin')
 let buildFolder = abs(env.proj, 'build')
 
 // Check folders
@@ -33,13 +39,121 @@ if (!found(buildFolder)) {
   alert('Error: File "storage-rules.txt" not found in build folder.')
 }
 
-// Step: Create firebase.json file
-let config = {
-  projects: {
-    'default': env.cfg.firebase.authDomain.substr(0, env.cfg.firebase.authDomain.indexOf('.firebaseapp.com'))
+// Function to copy files to firebase bin folder
+let prepareFiles = (callback) => {
+  alert('Firebase deployment preparation ongoing - please wait ...')
+  try {
+    fs.removeSync(abs(binFolder, 'build'))
+    if (env.arg.hosting === true) {
+      fs.copy(buildFolder, abs(binFolder, 'build'))
+    }
+    if (env.arg.hosting !== true && env.arg.database === true) {
+      fs.copy(abs(buildFolder, 'database-rules.json'), abs(binFolder, 'build/database-rules.json'))
+    }
+    if (env.arg.hosting !== true && env.arg.storage === true) {
+      fs.copy(abs(buildFolder, 'storage-rules.txt'), abs(binFolder, 'build/storage-rules.txt'))
+    }
+    callback()
+  } catch (err) {
+    alert('Error: Firebase deployment preparation failed.', 'issue')
   }
 }
-fs.writeJsonSync(abs(cacheFolder, 'firebase.json'), config)
 
-// Run
-alert('Firebase deployment ongoing - please wait ...')
+// Function to update config files
+let updateConfigFiles = (callback) => {
+  alert('Firebase configuration update ongoing - please wait ...')
+  try {
+    // Ensure firebase cache folder
+    fs.ensureDirSync(abs(binFolder))
+    // Create/update .firebaserc file
+    let rc = {
+      projects: {
+        'default': env.cfg.firebase.authDomain.substr(0, env.cfg.firebase.authDomain.indexOf('.firebaseapp.com'))
+      }
+    }
+    fs.writeJsonSync(abs(binFolder, '.firebaserc'), rc)
+    // Create/update firebase.json file
+    let config = {}
+    if (env.arg.database === true) {
+      config.database = {
+        rules: join('build', 'database-rules.json')
+      }
+    }
+    if (env.arg.storage === true) {
+      config.storage = {
+        rules: join('build', 'storage-rules.txt')
+      }
+    }
+    if (env.arg.hosting === true) {
+      config.hosting = {
+        public: 'build',
+        ignore: [
+          'database-rules.json',
+          'storage-rules.txt',
+          'icon.png'
+        ]
+      }
+    }
+    fs.writeJsonSync(abs(binFolder, 'firebase.json'), config)
+    // Callback
+    callback()
+  } catch (err) {
+    alert('Error: Firebase configuration update failed.', 'issue')
+  }
+}
+
+// Deployment functions
+let databaseDeployment = (callback) => {
+  if (env.arg.database === true) {
+    alert('Firebase database rules deployment ongoing - please wait ...')
+    cmd(binFolder, 'firebase deploy --only database', () => {
+      alert('Firebase database rules deployment done.')
+      callback()
+    }, () => {
+      alert('Error: Firebase database rules deployment failed.', 'issue')
+    })
+  } else {
+    callback()
+  }
+}
+let storageDeployment = (callback) => {
+  if (env.arg.storage === true) {
+    alert('Firebase storage rules deployment ongoing - please wait ...')
+    cmd(binFolder, 'firebase deploy --only storage', () => {
+      alert('Firebase storage rules deployment done.')
+      callback()
+    }, () => {
+      //alert('Error: Firebase storage rules deployment failed.', 'issue')
+    })
+  } else {
+    callback()
+  }
+}
+let hostingDeployment = (callback) => {
+  if (env.arg.hosting === true) {
+    alert('Firebase hosting deployment ongoing - please wait ...')
+    cmd(binFolder, 'firebase deploy --only hosting', () => {
+      alert('Firebase hosting deployment done.')
+      callback()
+    }, () => {
+      //alert('Error: Firebase hosting deployment failed.', 'issue')
+    })
+  } else {
+    callback()
+  }
+}
+
+// Login
+cmd(binFolder, 'firebase login', () => {
+  prepareFiles(() => {
+    updateConfigFiles(() => {
+      databaseDeployment(() => {
+        storageDeployment(() => {
+          hostingDeployment(() => {
+            alert('Firebase deployment done.')
+          })
+        })
+      })
+    })
+  })
+})
