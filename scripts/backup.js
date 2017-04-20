8b7711acfa047888680801c30c64a885f45ffaee
scriptPilot <mail@scriptPilot.de>
2017-04-20 21:27:10 +0200

    backup rework
    close #435

--- scripts/backup.js
+++ scripts/backup.js
@@ -16,45 +16,29 @@ let abs = require('path').resolve
 // Define Firebase bin folder
 let binFolder = abs(env.proj, 'node_modules/firebase-tools/bin')
 
+// Define project
+let project = env.cfg.firebase.authDomain.substr(0, env.cfg.firebase.authDomain.indexOf('.firebaseapp.com'))
+
 // Define filename
 let now = new Date()
 let dateStr = now.getFullYear() + '-' + (now.getMonth() < 9 ? '0' : '') + (now.getMonth() + 1) + '-' + (now.getDate() < 10 ? '0' : '') + now.getDate()
 
-// Steps
-let prepareFirebase = function (callback) {
-  alert('Preparing Firebase - please wait ...')
-  try {
-    // Create/update .firebaserc file
-    let rc = {
-      projects: {
-        'default': env.cfg.firebase.authDomain.substr(0, env.cfg.firebase.authDomain.indexOf('.firebaseapp.com'))
-      }
-    }
-    fs.writeJsonSync(abs(binFolder, '.firebaserc'), rc)
-    callback()
-  } catch (err) {
-    alert('Firebase preparation failed.', 'issue')
-  }
-}
-
 // Run
 alert('Firebase backup ongoing - please wait ...')
-prepareFirebase(function () {
-  cmd(binFolder, 'firebase login', function () {
-    alert('Firebase database backup ongoing - please wait ...')
-    cmd(binFolder, 'firebase database:get / >"' + abs(env.proj, 'snapshots/firebase-database-' + dateStr + '.json') + '"', function () {
-      alert('Firebase user backup ongoing - please wait ...')
-      cmd(binFolder, 'firebase auth:export "' + abs(env.proj, 'snapshots/firebase-users-' + dateStr + '.json') + '" --format=json', function () {
-        try {
-          let dbFile = fs.readJsonSync(abs(env.proj, 'snapshots/firebase-database-' + dateStr + '.json'))
-          fs.writeJsonSync(abs(env.proj, 'snapshots/firebase-database-' + dateStr + '.json'), dbFile)
-          let userFile = fs.readJsonSync(abs(env.proj, 'snapshots/firebase-users-' + dateStr + '.json'))
-          fs.writeJsonSync(abs(env.proj, 'snapshots/firebase-users-' + dateStr + '.json'), userFile)
-          alert('Firebase backup done.')
-        } catch (err) {
-          alert('Failed to beautify database backup files.', 'issue')
-        }
-      }, 'Firebase user backup failed.')
-    }, 'Firebase database backup failed.')
+cmd(binFolder, 'firebase login', function () {
+  alert('Firebase database backup ongoing - please wait ...')
+  cmd(binFolder, 'firebase database:get / >"' + abs(env.proj, 'snapshots/firebase-database-' + dateStr + '.json') + '" --project ' + project, function () {
+    alert('Firebase user backup ongoing - please wait ...')
+    cmd(binFolder, 'firebase auth:export "' + abs(env.proj, 'snapshots/firebase-users-' + dateStr + '.json') + '" --format=json --project ' + project, function () {
+      try {
+        let dbFile = fs.readJsonSync(abs(env.proj, 'snapshots/firebase-database-' + dateStr + '.json'))
+        fs.writeJsonSync(abs(env.proj, 'snapshots/firebase-database-' + dateStr + '.json'), dbFile)
+        let userFile = fs.readJsonSync(abs(env.proj, 'snapshots/firebase-users-' + dateStr + '.json'))
+        fs.writeJsonSync(abs(env.proj, 'snapshots/firebase-users-' + dateStr + '.json'), userFile)
+        alert('Firebase backup done.')
+      } catch (err) {
+        alert('Failed to beautify database backup files.', 'issue')
+      }
+    }, 'Firebase user backup failed.')
   }, 'Firebase login failed.')
 })
