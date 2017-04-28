78d53b173cc081c1a0e3e2117dcb44c14fcbdafa
scriptPilot <mail@scriptPilot.de>
2017-04-28 19:39:26 +0200

    Framework7-Vue update

--- vendor/framework7-vue/framework7-vue.js
+++ vendor/framework7-vue/framework7-vue.js
@@ -9,7 +9,7 @@
  * 
  * Licensed under MIT
  * 
- * Released on: April 27, 2017
+ * Released on: April 28, 2017
  */
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
@@ -655,7 +655,7 @@ var Page = {
           'no-toolbar': this.noToolbar,
           'no-tabbar': this.noTabbar,
           'tabs': this.tabs,
-          'no-swipeback': this.noSwipeBack
+          'no-swipeback': this.noSwipeback
         };
         if (this.theme) { co['theme-' + this.theme] = true; }
         if (this.layout) { co['layout-' + this.layout] = true; }
@@ -2400,7 +2400,7 @@ var FormInput = {
         multiple: self.multiple,
         readonly: self.readonly,
         required: self.required,
-        style: self.style,
+        style: self.inputStyle,
         color: self.color,
 	      pattern: self.pattern
       };
@@ -2511,7 +2511,7 @@ var FormInput = {
       multiple: Boolean,
       readonly: Boolean,
       required: Boolean,
-      style: String,
+      inputStyle: String,
 	    pattern: String,
       resizable: Boolean,
 
@@ -2668,7 +2668,7 @@ var FormInput = {
   };
 
 var FormSwitch = {
-render: function(){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('label',{staticClass:"label-switch",class:_vm.color ? 'color-' + _vm.color : '',on:{"click":_vm.onClick}},[_c('input',{style:(_vm.style),attrs:{"type":"checkbox","name":_vm.name,"id":_vm.id,"disabled":_vm.disabled,"readonly":_vm.readonly,"required":_vm.required},domProps:{"value":_vm.valueComputed,"checked":_vm.checkedComputed},on:{"input":_vm.onInput,"change":_vm.onChange}}),_vm._v(" "),_c('div',{staticClass:"checkbox"})])},
+render: function(){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('label',{staticClass:"label-switch",class:_vm.color ? 'color-' + _vm.color : '',on:{"click":_vm.onClick}},[_c('input',{style:(_vm.inputStyle),attrs:{"type":"checkbox","name":_vm.name,"id":_vm.id,"disabled":_vm.disabled,"readonly":_vm.readonly,"required":_vm.required},domProps:{"value":_vm.valueComputed,"checked":_vm.checkedComputed},on:{"input":_vm.onInput,"change":_vm.onChange}}),_vm._v(" "),_c('div',{staticClass:"checkbox"})])},
 staticRenderFns: [],
     props: {
       name: String,
@@ -2679,7 +2679,7 @@ staticRenderFns: [],
       disabled: Boolean,
       readonly: Boolean,
       required: Boolean,
-      style: String,
+      inputStyle: String,
 
       color: String
     },
@@ -2732,7 +2732,7 @@ staticRenderFns: [],
   };
 
 var FormRange = {
-render: function(){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"range-slider",class:_vm.color ? 'color-' + _vm.color : ''},[_c('input',{style:(_vm.style),attrs:{"type":"range","name":_vm.name,"id":_vm.id,"disabled":_vm.disabled,"readonly":_vm.readonly,"required":_vm.required,"max":_vm.max,"min":_vm.min,"step":_vm.step},domProps:{"value":_vm.value},on:{"input":_vm.onInput,"change":_vm.onChange,"click":_vm.onClick}})])},
+render: function(){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"range-slider",class:_vm.color ? 'color-' + _vm.color : ''},[_c('input',{style:(_vm.inputStyle),attrs:{"type":"range","name":_vm.name,"id":_vm.id,"disabled":_vm.disabled,"readonly":_vm.readonly,"required":_vm.required,"max":_vm.max,"min":_vm.min,"step":_vm.step},domProps:{"value":_vm.value},on:{"input":_vm.onInput,"change":_vm.onChange,"click":_vm.onClick}})])},
 staticRenderFns: [],
     props: {
       name: String,
@@ -2741,7 +2741,7 @@ staticRenderFns: [],
       disabled: Boolean,
       readonly: Boolean,
       required: Boolean,
-      style: String,
+      inputStyle: String,
       max: [String, Number],
       min: [String, Number],
       step: [String, Number],
@@ -4849,13 +4849,18 @@ var framework7Vue = {
         currentRoute,
         router;
 
-    function initFramework7(f7Params) {
+    function initFramework7(f7Params, root) {
       if (!window.Framework7) { return; }
       f7Params = f7Params || {};
 
       // Material
       if (typeof f7Params.material === 'undefined' && Vue.prototype.$theme.material) {
         f7Params.material = true;
+      }
+
+      // Root
+      if (typeof f7Params.root === 'undefined' && root) {
+        f7Params.root = root.$el;
       }
 
       // Init
@@ -4928,7 +4933,7 @@ var framework7Vue = {
           if (self.onF7Init) { self.onF7Init(f7Instance); }
         });
         if (self === self.$root) {
-          initFramework7(self.$options.framework7);
+          initFramework7(self.$options.framework7, self.$root);
         }
       },
       components: {
