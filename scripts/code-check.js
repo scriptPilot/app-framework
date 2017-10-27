9215bfebc2e01b2c5b3bdb2b0012caa0cc085f5a
scriptPilot <mail@scriptPilot.de>
2017-10-27 14:51:52 +0200

    add ESLint env: browser + node

--- scripts/code-check.js
+++ scripts/code-check.js
@@ -1,43 +1,41 @@
 /* Purpose: Check or fix scripts according Standard JS and create log file with remaining findings. */
 
-'use strict'
-
 // Load packages
-let env = require('./env')
-let alert = require('./alert')
-let cmd = require('./cmd')
-let fs = require('fs-extra')
-let abs = require('path').resolve
+const env = require('./env')
+const alert = require('./alert')
+const cmd = require('./cmd')
+const fs = require('fs-extra')
+const abs = require('path').resolve
 
 // Show message
-alert('Code ' + (env.arg.fix === true ? 'fix' : 'check') + ' ongoing - please wait ...')
+alert(`Code ${env.arg.fix === true ? 'fix' : 'check'} ongoing - please wait ...`)
 
 // Define log file
-let logFile = 'code-unconformities.log'
+const logFile = 'code-unconformities.log'
 
 // Define Standard parameters
-let params = [
+const params = [
   'node',
   'node_modules/eslint/bin/eslint.js',
   // Find app.vue, pages/*.vue and pages/sub/*.vue
-  '"' + abs(env.app, '**/*.vue') + '"',
-  '"' + abs(env.app, '**/*.js') + '"'
+  `"${abs(env.app, '**/*.vue')}"`,
+  `"${abs(env.app, '**/*.js')}"`
 ]
 if (!env.installed) {
-  params.push('"' + abs(__dirname, '../client/*.js') + '"')
-  params.push('"' + abs(__dirname, '../scripts/*.js') + '"')
+  params.push(`"${abs(__dirname, '../client/*.js')}"`)
+  params.push(`"${abs(__dirname, '../scripts/*.js')}"`)
 }
 // Print to log file
 params.push('>')
-params.push('"' + abs(env.proj, logFile) + '"')
+params.push(`"${abs(env.proj, logFile)}"`)
 if (env.arg.fix === true) {
   params.push('--fix')
 }
 
 // Define error alert
-let errorAlert = (env.arg.fix !== true ? 'Code unconformities found.' : 'Some unconformities must be fixed manually.') + '\n' +
-                 'Please check "' + logFile + '" for detailed information.\n' +
-                 (env.arg.fix !== true ? 'You can run "npm run fix" first for automatic fix.' : '')
+const errorAlert = `${env.arg.fix !== true ? 'Code unconformities found.' : 'Some unconformities must be fixed manually.'}\n` +
+                 `Please check "${logFile}" for detailed information.\n${
+                   env.arg.fix !== true ? 'You can run "npm run fix" first for automatic fix.' : ''}`
 
 // Update eslint config file
 const eslintConfig = env.cfg.eslint
@@ -45,6 +43,10 @@ if (eslintConfig.extends === 'airbnb') {
   eslintConfig.extends = 'airbnb-base'
 }
 eslintConfig.plugins = ['vue']
+eslintConfig.env = {
+  browser: true,
+  node: true
+}
 try {
   fs.writeJsonSync(abs(env.proj, '.eslintrc'), eslintConfig, { spaces: 2 })
 } catch (err) {
@@ -52,9 +54,9 @@ try {
 }
 
 // Do the fix
-cmd([env.proj], params, function () {
+cmd([env.proj], params, () => {
   fs.removeSync(abs(env.proj, logFile))
-  alert('Code ' + (env.arg.fix === true ? 'fix' : 'check') + ' done without findings.')
-}, function () {
+  alert(`Code ${env.arg.fix === true ? 'fix' : 'check'} done without findings.`)
+}, () => {
   alert(errorAlert, 'error')
 })
