b516d3e8e716cf335725dc41a477d838c8981fcb
scriptPilot <mail@scriptPilot.de>
2017-11-01 14:21:14 +0100

    eslint

--- scripts/path.js
+++ scripts/path.js
@@ -10,23 +10,23 @@ const isInstalled = fs.existsSync(path.resolve(__dirname, '../../../package.json
 // Export functions to get absolute path
 module.exports = {
   // Package
-  pkg(...args) {
+  pkg (...args) {
     return isInstalled ? path.resolve(__dirname, '../../../', ...args) : path.resolve(__dirname, '../', ...args)
   },
   // App folder
-  app(...args) {
+  app (...args) {
     return isInstalled ? path.resolve(__dirname, '../../../app', ...args) : path.resolve(__dirname, '../demo', ...args)
   },
   // Node modules folder
-  modules(...args) {
+  modules (...args) {
     return isInstalled ? path.resolve(__dirname, '../../../node_modules/', ...args) : path.resolve(__dirname, '../node_modules/', ...args)
   },
   // Cache folder
-  cache(...args) {
+  cache (...args) {
     return isInstalled ? path.resolve(__dirname, '../../../node_modules/.app-framework-cache/', ...args) : path.resolve(__dirname, '../node_modules/.app-framework-cache/', ...args)
   },
   // Scripts folder
-  scripts(...args) {
+  scripts (...args) {
     return isInstalled ? path.resolve(__dirname, '../../../node_modules/app-framework/scripts/', ...args) : path.resolve(__dirname, '../scripts/', ...args)
-  },
+  }
 }
