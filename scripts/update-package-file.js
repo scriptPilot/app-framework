92493917926ce8588fbe925b29087051f354e19e
ScriptPilot <mail@scriptpilot.de>
2018-12-30 01:09:16 +0100

    add missing version on postinstall

--- scripts/update-package-file.js
+++ scripts/update-package-file.js
@@ -11,6 +11,9 @@ const content = fs.readJsonSync(file);
 // Add missing name
 if (content.name === undefined) content.name = path.basename(path.project());
 
+// Add missing version
+if (content.version === undefined) content.version = '1.0.0';
+
 // Update scripts
 const prefix = `node ${isInstalled ? 'node_modules/app-framework/' : ''}scripts/`;
 const scriptArr = [
