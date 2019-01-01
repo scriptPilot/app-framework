8a3b6683f4ce8b049e6260895d44e93c74e4cc9c
ScriptPilot <mail@scriptpilot.de>
2019-01-02 00:11:15 +0100

    update package config update script

--- scripts/update-package-config.js
+++ scripts/update-package-config.js
@@ -2,8 +2,6 @@ const fs = require('fs-extra');
 const log = require('./helper/logger');
 const path = require('./helper/path');
 
-const isInstalled = path.project() !== path.framework();
-
 // Read file content
 const file = path.project('package.json');
 const content = fs.readJsonSync(file);
@@ -13,20 +11,6 @@ if (content.name === undefined) content.name = path.basename(path.project());
 
 // Add missing version
 if (content.version === undefined) content.version = '1.0.0';
-
-// Update scripts
-const prefix = `node ./${isInstalled ? 'node_modules/app-framework/' : ''}scripts/`;
-const scriptArr = [
-  'postinstall',
-  'test-eslint',
-  'dev',
-  'build',
-];
-const scriptObj = {};
-scriptArr.forEach((script) => {
-  scriptObj[script] = prefix + script;
-});
-content.scripts = scriptObj;
 
 // Write updated content
 fs.writeJsonSync(file, content, { spaces: 2 });
