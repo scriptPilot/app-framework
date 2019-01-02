477816acd3ebc0253af85713e0a6e9bec28ce1de
ScriptPilot <mail@scriptpilot.de>
2019-01-02 01:40:25 +0100

    fixed paths

--- scripts/test-eslint.js
+++ scripts/test-eslint.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const opn = require('opn');
 const fs = require('fs-extra');
+const { exec } = require('shelljs');
 const log = require('./helper/logger');
-const run = require('./helper/run');
 
 const logFileName = 'ESLint.log.html';
 const logFile = path.project(logFileName);
@@ -10,8 +10,9 @@ const cacheFile = path.cache('eslint/.eslintcache');
 const appConfigFile = path.app('config.json');
 
 const appConfig = fs.readJsonSync(appConfigFile);
+const configFile = path.project('.eslintrc.json');
 
-run.silent(`npx eslint . --ext .js --ext .vue --fix --output-file "${logFile}" --format html --cache --cache-location "${cacheFile}"`, (error) => {
+exec(`npx eslint "${path.project()}" --ignore-pattern "/node_modules/" --ext .js --ext .vue --fix --config "${configFile}" --output-file "${logFile}" --format html --cache --cache-location "${cacheFile}"`, { cwd: path.project(), silent: true }, (error) => {
   if (error) {
     opn(logFile, { wait: false });
     log.error(`Failed ESLint test. Please open ${logFileName} for details.`);
