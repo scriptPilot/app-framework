2825f0a0f0a517593d411fb1230a9ffeafe9852f
ScriptPilot <mail@scriptpilot.de>
2019-01-03 21:58:30 +0100

    optimized script execution

--- scripts/test.js
+++ scripts/test.js
@@ -5,14 +5,12 @@ const path = require('./helper/path');
 
 const config = fs.readJsonSync(path.app('config.json'));
 
-const tests = [];
-if (config.test.eslint.runOnTestCommand) tests.push(`node "${path.scripts('test-eslint.js')}"`);
-if (config.test.jest.runOnTestCommand) tests.push(`node "${path.scripts('test-jest.js')}"`);
-
-if (tests.length > 0) {
-  const scriptResult = run.loud(tests.join(' && '));
-  if (scriptResult.code === 0) log.success('Passed all tests.');
-  else log.error('Failed one or more tests.');
-} else {
-  log.info('No tests configured.');
+if (config.test.eslint.runOnTestCommand) {
+  if (run.script('test-eslint').code !== 0) process.exit(1);
 }
+
+if (config.test.jest.runOnTestCommand) {
+  if (run.script('test-jest').code !== 0) process.exit(1);
+}
+
+log.success('Passed all tests.');
