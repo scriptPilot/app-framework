27e3c9fb50919dace4032de4b4a97677642d96ae
ScriptPilot <mail@scriptpilot.de>
2019-01-03 18:49:56 +0100

    remove build path

--- scripts/helper/path.js
+++ scripts/helper/path.js
@@ -5,13 +5,12 @@ const isInstalled = fs.pathExistsSync(path.resolve(__dirname, '../../../../packa
 
 // Framework related shortcuts
 path.framework = (...nav) => path.resolve(__dirname, '../../', ...nav);
-path.templates = (...nav) => path.resolve(__dirname, '../../templates', ...nav);
-path.scripts = (...nav) => path.resolve(__dirname, '../', ...nav);
 path.cache = (...nav) => path.resolve(__dirname, '../../.cache', ...nav);
+path.scripts = (...nav) => path.resolve(__dirname, '../', ...nav);
+path.templates = (...nav) => path.resolve(__dirname, '../../templates', ...nav);
 
 // Project related shortcuts
 path.project = (...nav) => path.resolve(__dirname, isInstalled ? '../../../../' : '../../', ...nav);
 path.app = (...nav) => path.resolve(__dirname, isInstalled ? '../../../../app' : '../../templates/app', ...nav);
-path.build = (...nav) => path.resolve(__dirname, isInstalled ? '../../../../build' : '../../build', ...nav);
 
 module.exports = path;
