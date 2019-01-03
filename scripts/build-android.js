0dfe749727795a3f167f8cd50e7dfcfdb95d5772
ScriptPilot <mail@scriptpilot.de>
2019-01-03 14:26:22 +0100

    android build
    closes #824

---
+++ scripts/build-android.js
@@ -0,0 +1,31 @@
+// Import modules
+const fs = require('fs-extra');
+const log = require('./helper/logger');
+const path = require('./helper/path');
+const run = require('./helper/run');
+
+// Install Capacitor
+if (run.script('install-capacitor').code !== 0) process.exit(1);
+
+// Update Capacitor configuration
+if (run.script('update-capacitor-config').code !== 0) process.exit(1);
+
+// Add Android project folder
+const androidProjectFolder = path.project('android');
+if (fs.pathExistsSync(androidProjectFolder)) {
+  log.info('Android project folder already exists.');
+} else {
+  log.warning('Adding Android project folder - this may take a while ...');
+  const addAndroidScript = run.loud('npx cap add android');
+  if (addAndroidScript.code === 0) log.success('Added Android project folder.');
+  else log.error('Failed to add Android project folder.');
+}
+
+// Updating the Android folder
+log.warning('Updating the Android project folder - this may take a while ...');
+const updateScript = run.loud('npx cap sync android');
+if (updateScript.code === 0) log.success('Updated the Android project folder.');
+else log.error('Failed to update the Android project folder.');
+
+// Open Xcode
+run.loud('npx cap open android');
--- scripts/build.js
+++ scripts/build.js
@@ -14,6 +14,7 @@ scripts.push('build-icons');
 scripts.push('build-web');
 scripts.push('build-pwa');
 scripts.push('build-ios');
+scripts.push('build-android');
 
 const runNextScript = () => {
   if (scripts.length > 0) {
