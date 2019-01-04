ba7ab71260e6f09d19b2dbdff4d1fd72584ba9e8
ScriptPilot <mail@scriptpilot.de>
2019-01-04 12:56:34 +0100

    hide vue ip generally

--- templates/main.js
+++ templates/main.js
@@ -4,9 +4,7 @@ import 'framework7/css/framework7.css';
 import Framework7Vue from 'framework7-vue/framework7-vue.esm.bundle';
 import App from './app/app.vue';
 
-if (process.env.NODE_ENV === 'development') {
-  Vue.config.productionTip = false;
-}
+Vue.config.productionTip = false;
 
 Framework7.use(Framework7Vue);
 
