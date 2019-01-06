4a725f932b9ef501bc50945032447b8bdb62b399
ScriptPilot <mail@scriptpilot.de>
2019-01-06 12:47:53 +0100

    app config reset on prepublish
    closes #861

---
+++ scripts/prepublish.js
@@ -0,0 +1,19 @@
+// Import modules
+const fs = require('fs-extra');
+const json = require('json-schema-fix');
+const log = require('./helper/logger');
+const path = require('./helper/path');
+
+// Cancel if no App Framework development mode
+if (!fs.pathExistsSync(path.project('.enableDevelopmentMode'))) process.exit(0);
+
+// Reset templates/app/config.json to default values according schema
+try {
+  const schemaFile = path.templates('app-config-schema.json');
+  const templateConfigFile = path.templates('app/config.json');
+  const schema = fs.readJsonSync(schemaFile);
+  fs.outputJsonSync(templateConfigFile, json.create(schema), { spaces: 2 });
+  log.success('Reset templates/app/config.json file.');
+} catch (e) {
+  log.error('Failed to reset the templates/app/config.json file.');
+}
