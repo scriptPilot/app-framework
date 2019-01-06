5129d1e232749089183e41b7efc8c44d093b71c7
ScriptPilot <mail@scriptpilot.de>
2019-01-06 13:15:28 +0100

    check changelog before publishing, use new prepublishOnly

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# App Framework - Changelog
+
+## App Framework v3
+
+### App Framework v3.0.0-beta.35
+
+Current development version. Please check the [milestone](https://github.com/scriptPilot/app-framework/milestone/8) for details.
+
+## App Framework v2
+
+App Framework v2 was never released.
+
+## App Framework v1
+
+Please see the [changelog for v1](https://github.com/scriptPilot/app-framework/blob/v1/CHANGELOG.md).
--- scripts/prepublishOnly.js
+++ scripts/prepublishOnly.js
@@ -21,3 +21,20 @@ try {
 
 // Run tests
 if (run.script('test').code !== 0) process.exit(1);
+// Check changelog update
+try {
+  const { version } = fs.readJsonSync(path.framework('package.json'))
+  const changelogFile = path.framework('CHANGELOG.md');
+  const changelogFileContent = fs.readFileSync(changelogFile, { encoding: 'utf-8' });
+  const searchRegExp = new RegExp(`\\n### App Framework v${version}`, 'g')
+  const searchRes = changelogFileContent.match(searchRegExp)
+  if (searchRes === null) {
+    log.error('No changelog entry found for version ' + version + '.')
+  } else if (searchRes.length === 1) {
+    log.success('Changelog entry found for version ' + version + '.')
+  } else {
+    log.error(searchRes.length + ' changelog entries found for version ' + version + '.')
+  }
+} catch (e) {
+  log.error('Failed to check the changelog.');
+}
