31381ec46ecc2f9df401392dc0b6cda748637118
ScriptPilot <mail@scriptpilot.de>
2019-01-06 12:48:21 +0100

    run tests in prepublish
    closes #862

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -3,6 +3,7 @@ const fs = require('fs-extra');
 const json = require('json-schema-fix');
 const log = require('./helper/logger');
 const path = require('./helper/path');
+const run = require('./helper/run');
 
 // Cancel if no App Framework development mode
 if (!fs.pathExistsSync(path.project('.enableDevelopmentMode'))) process.exit(0);
@@ -17,3 +18,6 @@ try {
 } catch (e) {
   log.error('Failed to reset the templates/app/config.json file.');
 }
+
+// Run tests
+if (run.script('test').code !== 0) process.exit(1);
