46bf2d9dba50d7fdb81ebb3cc313af0f6ea053e8
ScriptPilot <mail@scriptpilot.de>
2019-01-06 12:48:38 +0100

    eslint fix

--- cli/app-deploy.js
+++ cli/app-deploy.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const { execSync } = require('child_process')
-const path = require('../scripts/helper/path')
+const { execSync } = require('child_process');
+const path = require('../scripts/helper/path');
 
 program
   .option('--ftp', 'deploy PWA to FTP server')
   .parse(process.argv);
 
-  if (program.ftp) execSync('node ./deploy-ftp.js', { cwd: path.scripts(), stdio: 'inherit' })
+if (program.ftp) execSync('node ./deploy-ftp.js', { cwd: path.scripts(), stdio: 'inherit' });
