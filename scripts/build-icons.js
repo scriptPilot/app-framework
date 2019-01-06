713dada308635f20c45f34e86b533026c8919241
ScriptPilot <mail@scriptpilot.de>
2019-01-06 15:03:27 +0100

    icon build improvement
    closes #845 , closes #849

--- scripts/build-icons.js
+++ scripts/build-icons.js
@@ -1,36 +1,40 @@
+// Import modules
 const fs = require('fs-extra');
 const jimp = require('jimp');
 const path = require('./helper/path');
 const log = require('./helper/logger');
 
-const sourceFile = path.app('icon.png');
-const iconFolder = path.cache('icons');
+// Load app configuration
+const configFile = path.app('config.json');
+let config = {};
+try {
+  config = fs.readJsonSync(configFile);
+  log.success('Loaded app config file.');
+} catch (e) {
+  log.error('Failed to load app config file.');
+}
 
-const clearIconFolder = (callback) => {
-  fs.emptyDir(iconFolder, () => {
-    log.success('Icon folder cleaned-up.');
-    callback();
-  });
+// Build icons
+const buildIcons = async () => {
+  // Build PWA icons
+  if (config.pwa.buildOnBuildCommand) {
+    try {
+      // Empty cache folder
+      fs.emptyDirSync(path.cache('icons/pwa'));
+      // Create PWA icons
+      let image;
+      image = await jimp.read(path.app(config.pwa.iconFile));
+      image.resize(512, 512).write(path.cache('icons/pwa/icon-512px.png'));
+      image = await jimp.read(path.app(config.pwa.iconFile));
+      image.resize(192, 192).write(path.cache('icons/pwa/icon-192px.png'));
+      image = await jimp.read(path.app(config.pwa.iconFile));
+      image.resize(180, 180).write(path.cache('icons/pwa/apple-touch-icon.png'));
+      // Log success
+      log.success('Created PWA icons.');
+    } catch (e) {
+      log.debug(e);
+      log.error('Failed to create PWA icons.');
+    }
+  }
 };
-
-const createIcon = (name, size, callback) => {
-  const destFile = path.resolve(iconFolder, name);
-  jimp.read(sourceFile, (readError, iconFile) => {
-    iconFile.resize(size, size, (resizeError, resizedFile) => {
-      resizedFile.write(destFile, () => {
-        log.success(`Icon ${name} created.`);
-        callback();
-      });
-    });
-  });
-};
-
-clearIconFolder(() => {
-  createIcon('pwa/icon-512px.png', 512, () => {
-    createIcon('pwa/icon-192px.png', 192, () => {
-      createIcon('pwa/apple-touch-icon.png', 180, () => {
-        log.success('Icons updated.');
-      });
-    });
-  });
-});
+buildIcons();
