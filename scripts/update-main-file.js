4118400c3b6818594e3fde95e96959cdb07f3bd5
ScriptPilot <mail@scriptpilot.de>
2019-01-09 22:21:12 +0100

    make icon font usage configurable
    closes #826

--- scripts/update-main-file.js
+++ scripts/update-main-file.js
@@ -3,11 +3,25 @@ const fs = require('fs-extra');
 const log = require('./helper/logger');
 const path = require('./helper/path');
 
+// Load app configuration
+const configFile = path.app('config.json');
+let config = {};
+try {
+  config = fs.readJsonSync(configFile);
+  log.success('Loaded app config file.');
+} catch (e) {
+  log.error('Failed to load app config file.');
+}
+
 // Define variables
 const relAppPath = path.relative(path.cache(), path.app());
 const importVue = fs.pathExistsSync(path.app('vue.js'))
   ? `import VueDefault from 'vue'; import customizeVue from '${relAppPath}/vue.js'; const Vue = customizeVue(VueDefault);`
   : 'import Vue from \'vue\'';
+const importFramework7Icons = config.iconFonts.useFramework7Icons
+  ? 'import \'framework7-icons\';' : '';
+const importMaterialDesignIcons = config.iconFonts.useMaterialDesignIcons
+  ? 'import \'material-icons/iconfont/material-icons.css\';' : '';
 const importAppCSSFile = fs.pathExistsSync(path.app('app.css'))
   ? `import "${relAppPath}/app.css";` : '';
 
@@ -16,12 +30,12 @@ const mainFileContent = `
 ${importVue}
 import Framework7 from 'framework7/framework7.esm.bundle';
 import 'framework7/css/framework7.css';
+${importFramework7Icons}
+${importMaterialDesignIcons}
 import Framework7Vue from 'framework7-vue/framework7-vue.esm.bundle';
 import { Plugins } from '@capacitor/core';
-import App from '${relAppPath}/app.vue';
-import 'framework7-icons';
-import 'material-icons/iconfont/material-icons.css';
 ${importAppCSSFile}
+import App from '${relAppPath}/app.vue';
 
 Vue.config.productionTip = false;
 
