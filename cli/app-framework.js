54ef28cd01902c7a7dbf30151df200fc6e421414
ScriptPilot <mail@scriptpilot.de>
2019-05-06 00:45:11 +0200

    work on v4

--- cli/app-framework.js
+++ cli/app-framework.js
@@ -4,88 +4,45 @@ const program = require('commander');
 const { exec } = require('shelljs');
 const { resolve } = require('path')
 const List = require('prompt-list')
+const prompts = require('prompts')
 
-function getJSFramework(cb) {
-  const list = new List({
-    message: 'Which JavaScript Framework do you want to use?',
+async function testPrompts() {
+  const response = await prompts([{
+    type: 'select',
+    name: 'color',
+    message: 'Pick colors',
     choices: [
-      'Vue',
-      {name: 'React', disabled: 'Planned for the future'},
-      'No Framework'
-    ]
-  })
-  list.ask((JSFramework) => cb)
+      { title: 'Red', value: '#ff0000' },
+      { title: 'Green', value: '#00ff00' },
+      { title: 'Blue', value: '#0000ff' }
+    ],
+  },{
+  type: null,
+  name: 'forgetme',
+  message: `I'll never be shown anyway`,
+},{
+        type: 'text',
+        name: 'username',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'number',
+        name: 'age',
+        message: 'How old are you?'
+    },
+    {
+        type: 'text',
+        name: 'about',
+        message: 'Tell something about yourself',
+        initial: 'Why should I?'
+    }]);
+
+  console.log(response); // => { value: 24 }
 }
 
 program
-  .command('init')
   .action(() => {
-    const list = new List({
-      message: 'Which JavaScript Framework do you want to use?',
-      choices: [
-        'Vue',
-        {name: 'React', disabled: 'Planned for the future'},
-        'No Framework'
-      ]
-    })
-    list.ask((JSFramework) => {
-      const list = new List({
-        message: 'Which UX Framework do you want to use?',
-        choices: [
-          'Framework7',
-          'Material',
-          'Bootstrap',
-          'No Framework'
-        ]
-      })
-      list.ask((UXFramework) => {
-        console.log(JSFramework, UXFramework)
-      })
-    })
+    testPrompts()
   })
-
-/*
-
-console.log('CWD', resolve())
-console.log('CWD FW', resolve('framework'))
-console.log('DIRNAME', __dirname)
-console.log('DEVELOPMENT', resolve('framework') === __dirname)
-
-function run(scriptName) {
-  const scriptFile = resolve(__dirname, 'scripts', scriptName)
-  exec(`node ${scriptFile}`)
-}
-
-*/
-
-program
-  .command('dev')
-  .action(() => {
-    run('fixCode');
-    run('fixConfig');
-    run('buildAppScript');
-    run('buildIndexFile');
-    exec(`npx concurrently --kill-others "node ${__dirname}/watchAppConfig" "node ${__dirname}/buildDev"`);
-  });
-
-program
-  .command('dev-electron')
-  .action(() => {
-    run('fixCode');
-    run('fixConfig');
-    run('buildAppScript');
-    run('buildIndexFile');
-    exec(`npx concurrently --kill-others "node ${__dirname}/watchAppConfig" "node ${__dirname}/buildDevElectron"`);
-  });
-
-program
-  .command('build')
-  .action(() => {
-    run('fixCode');
-    run('fixConfig');
-    run('buildAppScript');
-    run('buildIndexFile');
-    run('buildWeb');
-  });
 
 program.parse(process.argv);
