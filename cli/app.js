d0bf07614a6d9b3c3e7ef966df1e7ece69e769c2
ScriptPilot <mail@scriptpilot.de>
2019-05-06 00:02:04 +0200

    work on v4

---
+++ cli/app.js
@@ -0,0 +1,91 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const { exec } = require('shelljs');
+const { resolve } = require('path')
+const List = require('prompt-list')
+
+function getJSFramework(cb) {
+  const list = new List({
+    message: 'Which JavaScript Framework do you want to use?',
+    choices: [
+      'Vue',
+      {name: 'React', disabled: 'Planned for the future'},
+      'No Framework'
+    ]
+  })
+  list.ask((JSFramework) => cb)
+}
+
+program
+  .command('init')
+  .action(() => {
+    const list = new List({
+      message: 'Which JavaScript Framework do you want to use?',
+      choices: [
+        'Vue',
+        {name: 'React', disabled: 'Planned for the future'},
+        'No Framework'
+      ]
+    })
+    list.ask((JSFramework) => {
+      const list = new List({
+        message: 'Which UX Framework do you want to use?',
+        choices: [
+          'Framework7',
+          'Material',
+          'Bootstrap',
+          'No Framework'
+        ]
+      })
+      list.ask((UXFramework) => {
+        console.log(JSFramework, UXFramework)
+      })
+    })
+  })
+
+/*
+
+console.log('CWD', resolve())
+console.log('CWD FW', resolve('framework'))
+console.log('DIRNAME', __dirname)
+console.log('DEVELOPMENT', resolve('framework') === __dirname)
+
+function run(scriptName) {
+  const scriptFile = resolve(__dirname, 'scripts', scriptName)
+  exec(`node ${scriptFile}`)
+}
+
+*/
+
+program
+  .command('dev')
+  .action(() => {
+    run('fixCode');
+    run('fixConfig');
+    run('buildAppScript');
+    run('buildIndexFile');
+    exec(`npx concurrently --kill-others "node ${__dirname}/watchAppConfig" "node ${__dirname}/buildDev"`);
+  });
+
+program
+  .command('dev-electron')
+  .action(() => {
+    run('fixCode');
+    run('fixConfig');
+    run('buildAppScript');
+    run('buildIndexFile');
+    exec(`npx concurrently --kill-others "node ${__dirname}/watchAppConfig" "node ${__dirname}/buildDevElectron"`);
+  });
+
+program
+  .command('build')
+  .action(() => {
+    run('fixCode');
+    run('fixConfig');
+    run('buildAppScript');
+    run('buildIndexFile');
+    run('buildWeb');
+  });
+
+program.parse(process.argv);
---
+++ framework/scripts/postinstall.js
@@ -0,0 +1,6 @@
+const { copySync } = require('fs-extra')
+const { resolve } = require('path')
+
+console.log('INIT CWD', process.env.INIT_CWD);
+console.log('RESOLVE', resolve())
+copySync(resolve(__dirname, '../../app'), resolve('app'))
